5e12abcbd215acec3046ea3768abfac15678f359
adametrnal <brill.adam@gmail.com>
2021-11-24 22:44:43 -0500

    update project id

--- programs/solana-project/src/lib.rs
+++ programs/solana-project/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9qKWyzr36SvhEw6RjDQL1c9NWCdaMwXy2gDULPEyJY7D");
 
 #[program]
 pub mod solana_project {
